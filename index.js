38d43fa351040442c032b93c1f3bf60f5213df62
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-15 07:59:56 -0300

    Juntando listas 2/2

--- index.js
+++ index.js
@@ -5,17 +5,24 @@ function juntaListas(lista1, lista2) {
 
     let posicaoAtualLista1 = 0;
     let posicaoAtualLista2 = 0;
-
     let atual = 0;
 
     while(posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length) {
-        let produtoAtualLista1 = lista1[produtoAtualLista1];
-        let produtoAtualLista2 = lista2[produtoAtualLista2];
+        let produtoAtualLista1 = lista1[posicaoAtualLista1];
+        let produtoAtualLista2 = lista2[posicaoAtualLista2];
+        // console.log(`comparando ${produtoAtualLista1.titulo} com ${produtoAtualLista2.titulo}`)
 
         if(produtoAtualLista1.preco < produtoAtualLista2.preco) {
-            listaFinal[atual];
+            listaFinal[atual] = produtoAtualLista1;
+            posicaoAtualLista1++;
+        } else {
+            listaFinal[atual] = produtoAtualLista2;
+            posicaoAtualLista2++;
         }
+        atual++
     }
 
     return listaFinal;
 }
+
+console.log(juntaListas(edFolha, edGalho));
