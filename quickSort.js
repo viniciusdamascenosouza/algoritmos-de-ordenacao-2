885fa17d637b5820bafdfece971b0a78bc53f7ea
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-03-06 16:54:51 -0300

    Adicionando ao quick sort

---
+++ quickSort.js
@@ -0,0 +1,10 @@
+const listaLivros = require('./arrays2');
+
+function quickSort(array, esquerda, direita) {
+    if (array.length > 1) {
+        
+    }
+    return array;
+}
+
+console.log(quickSort(listaLivros, 0, listaLivros.length - 1));
